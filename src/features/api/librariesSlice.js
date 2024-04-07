ec5bd00a08f930fd51d220090bf264a78aa7d509
Omar Faruk <omar.faruk@outlook.com>
2024-04-07 21:07:58 +0100

    Fixed fetching libraries after login

--- src/features/api/librariesSlice.js
+++ src/features/api/librariesSlice.js
@@ -1,12 +1,12 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
+import { createApi } from "@reduxjs/toolkit/query/react";
 
-import { axiosBaseQuery } from '../../helpers/axios.helpers'
+import { axiosBaseQuery } from "../../helpers/axios.helpers";
 
-import { parseResponse, removeLinks } from '../../helpers/parseResponse';
+import { parseResponse, removeLinks } from "../../helpers/parseResponse";
 
 // ----------------------------------------------
 export const librariesApi = createApi({
-    reducerPath: 'libraries',
+    reducerPath: "libraries",
     baseQuery: axiosBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
     endpoints: (builder) => ({
         getLibraries: builder.query({
@@ -15,71 +15,78 @@ export const librariesApi = createApi({
                 pageNumber = 1,
                 pageSize = 12,
                 sortBy = null,
-                sortDirection = null
+                sortDirection = null,
             }) => {
-                let queryVal = query ? `&query=${query}` : '';
+                let queryVal = query ? `&query=${query}` : "";
                 if (sortBy) {
                     queryVal += `&sortBy=${sortBy}`;
                 }
                 if (sortDirection) {
                     queryVal += `&sortDirection=${sortDirection}`;
                 }
-                return ({ url: `/libraries?pageNumber=${pageNumber}&pageSize=${pageSize}${queryVal}`, method: 'get' });
+                return {
+                    url: `/libraries?pageNumber=${pageNumber}&pageSize=${pageSize}${queryVal}`,
+                    method: "get",
+                };
             },
             transformResponse: (response) => parseResponse(response),
-            providesTags: [ 'Libraries' ]
+            providesTags: ["Libraries"],
         }),
         getLibrary: builder.query({
-            query: ({ libraryId }) => ({ url: `/libraries/${libraryId}`, method: 'get' }),
+            query: ({ libraryId }) => ({
+                url: `/libraries/${libraryId}`,
+                method: "get",
+            }),
             transformResponse: (response) => parseResponse(response),
-            providesTags: [ 'Libraries' ]
+            providesTags: ["Libraries"],
         }),
         addLibrary: builder.mutation({
             query: ({ library }) => ({
                 url: `/libraries`,
-                method: 'POST',
-                data: removeLinks(library)
+                method: "POST",
+                data: removeLinks(library),
             }),
-            invalidatesTags: [ 'Libraries' ]
+            invalidatesTags: ["Libraries"],
         }),
         updateLibrary: builder.mutation({
             query: ({ library }) => ({
                 url: library.links.update,
-                method: 'PUT',
-                data: removeLinks(library)
+                method: "PUT",
+                data: removeLinks(library),
             }),
-            invalidatesTags: [ 'Libraries' ]
+            invalidatesTags: ["Libraries"],
         }),
         deleteLibrary: builder.mutation({
             query: ({ library }) => ({
                 url: library.links.delete,
-                method: 'DELETE'
+                method: "DELETE",
             }),
-            invalidatesTags: [ 'Libraries' ]
+            invalidatesTags: ["Libraries"],
         }),
         updateLibraryImage: builder.mutation({
             query: ({ library, payload }) => {
                 const formData = new FormData();
-                formData.append('file', payload, payload.fileName);
-                return ({
+                formData.append("file", payload, payload.fileName);
+                return {
                     url: library.links.image_upload,
-                    method: 'PUT',
+                    method: "PUT",
                     data: formData,
                     formData: true,
                     headers: {
-                        'content-type': 'multipart/form-data'
-                    }
-                });
+                        "content-type": "multipart/form-data",
+                    },
+                };
             },
-            invalidatesTags: [ 'Libraries' ]
+            invalidatesTags: ["Libraries"],
         }),
     }),
-})
+});
 
-
-export const { useGetLibrariesQuery,
+export const {
+    useGetLibrariesQuery,
     useGetLibraryQuery,
     useAddLibraryMutation,
     useUpdateLibraryMutation,
     useDeleteLibraryMutation,
-    useUpdateLibraryImageMutation } = librariesApi
+    useUpdateLibraryImageMutation,
+} = librariesApi;
