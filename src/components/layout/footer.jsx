e41a32af446b3a12189bfc1701f87b04a77c05a8
Omar Faruk <omar.faruk@outlook.com>
2024-05-23 21:43:46 +0100

    Fixed image loading issue

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -3,14 +3,17 @@ import { useTranslation } from 'react-i18next';
 
 // 3rd party imports
 import { Layout } from 'antd';
+
+// Local imports
 import './styles.scss';
+import { NODE_ENV } from "env";
 
 // ----------------------------------------------
 
 const Footer = () => {
     const { t } = useTranslation();
     const contents = (<div className="footer">
-        <div className="footer__copyrights">{t('footer.copyrights')}</div>
+        <div className="footer__copyrights">{t('footer.copyrights')}{NODE_ENV != 'production' ? `- (${NODE_ENV})` : null}</div>
     </div>)
     return (<Layout.Footer style={{ textAlign: 'center' }}>{contents}</Layout.Footer>)
 }
