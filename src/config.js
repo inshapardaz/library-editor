20899824b6402707a3ad66527a84c6b2061c9390
Umer Farooq <omar.faruk@outlook.com>
2024-07-02 09:15:19 +0100

    Fixed the environment matching

--- src/config.js
+++ src/config.js
@@ -4,13 +4,17 @@ let API_URL = 'http://localhost:4000';
 console.log('--------------------------------------------------')
 console.log('window.location.host:', window.location.host);
 
-if (window.location.host.toLocaleLowerCase() === 'nawihta.dev') {
+if (window.location.host.toLocaleLowerCase() == 'editor.nawishta.dev') {
     NODE_ENV = 'development';
     API_URL = 'http://api.nawistha.dev';
-} else if (window.location.host.toLocaleLowerCase() === 'editor.nawishta.co.uk' > 0) {
+    console.log('Environment is: development');
+} else if (window.location.host.toLocaleLowerCase() == 'editor.nawishta.co.uk' > 0) {
     NODE_ENV = 'development';
     API_URL = 'https://api.nawistha.co.uk';
-}
+    console.log('Environment is: production');
+} else{
+    console.log('Environment is: local');
+} 
 console.log('--------------------------------------------------')
 
 export {
