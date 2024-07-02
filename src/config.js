52336afe88745866781c3bc06b0f74df9b17cf04
Umer Farooq <omar.faruk@outlook.com>
2024-07-02 09:23:04 +0100

    Fixed issues with misspelled domain

--- src/config.js
+++ src/config.js
@@ -6,11 +6,11 @@ console.log('window.location.host:', window.location.host);
 
 if (window.location.host.toLocaleLowerCase() == 'editor.nawishta.dev') {
     NODE_ENV = 'development';
-    API_URL = 'http://api.nawistha.dev';
+    API_URL = 'http://api.nawishta.dev';
     console.log('Environment is: development');
 } else if (window.location.host.toLocaleLowerCase() == 'editor.nawishta.co.uk') {
     NODE_ENV = 'production';
-    API_URL = 'https://api.nawistha.co.uk';
+    API_URL = 'https://api.nawishta.co.uk';
     console.log('Environment is: production');
 } else{
     console.log('Environment is: local');
