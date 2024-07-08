ecb3111182ec5c5aaa7758c58e02a480d438b6f3
Umer Farooq <omar.faruk@outlook.com>
2024-07-08 08:28:29 +0100

    Fixed author card error

--- src/components/author/authorCard.jsx
+++ src/components/author/authorCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 // 3rd Party Libraries
 import { Card, Typography } from "antd";
