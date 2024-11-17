ace4e61578a8dc39c76ac1b662d06cedb60e3a7f
Omar Faruk <omar.faruk@outlook.com>
2024-11-17 11:33:27 +0000

    Made cookie use correct domain

--- src/domain/userRepository.js
+++ src/domain/userRepository.js
@@ -1,5 +1,6 @@
 import { isJsonString } from "/src/util";
 import Cookies from 'js-cookie';
+import { COOKIE_DOMAIN } from "/src/config";
 
 export const getUser = () => {
   const userCookie = Cookies.get('user');
@@ -11,7 +12,7 @@ export const getUser = () => {
 };
 
 export const setUser = (user) => {
-  Cookies.set('user', JSON.stringify(user));
+  Cookies.set('user', JSON.stringify(user), { path: '/', domain: COOKIE_DOMAIN, secure: true });
 };
 
 export const clearUser = () => {
